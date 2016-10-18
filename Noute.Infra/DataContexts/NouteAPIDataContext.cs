b2b600aec3563cc29fe6e30573cba6548dfe8ac9
Ruan Gervasi <ruan@ruangervasi.com.br>
2016-10-18 08:24:39 -0200

    StartProject

---
+++ Noute.Infra/DataContexts/NouteAPIDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using NouteAPI.Domain.Entities;
+
+namespace Noute.Infra.DataContexts
+{
+    public class NouteAPIDataContext : DbContext
+    {
+        public NouteAPIDataContext(DbContextOptions<NouteAPIDataContext> options) 
+            : base(options)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<NouteAPIDataContext>();
+            optionsBuilder.UseSqlServer(Microsoft.IdentityModel.Protocols.ConfigurationManager.ConnectionStrings)
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ NouteAPI.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NouteAPI.Domain.Entities
+{
+    public class User
+    {
+        public User()
+        {
+            this.RegistrationDate = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
